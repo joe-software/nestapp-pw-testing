76728dc9506c3f554a686e911a953df4a78d3b96
joe-software <joewebdev@outlook.com>
2024-10-29 22:06:29 +0000

    Written e2e interface tests for User module testing adding, editing and deleting data

--- tests/nest-app.spec.ts
+++ tests/nest-app.spec.ts
@@ -66,16 +66,61 @@ test('Testing Cars module CRUD functionality on the interface', async ({ page })
 
 });
 
+// Test functionality of Users module CRUD functionality on the interface
+test('Testing Users module CRUD functionality on the interface', async ({ page }) => {
+
+  // add user tests
+  await page.goto('http://localhost:3000/Users/add');
+  await expect(page).toHaveTitle('Add User Page');
+  await page.getByLabel('Add username: ').fill('PW username Test')
+  await page.getByLabel('Add bio: ').fill('PW bio Test')
+  await page.getByLabel('Add age: ').fill('12341234')
+  // await page.getByPlaceholder('Type here to add car model...').fill('PW Model Test')
+  await page.getByText('Click here to submit').click()
+  await expect(page.getByText('added')).toBeVisible()
+
+  await page.goto('http://localhost:3000/Users');
+  await expect(page.getByText('PW username Test')).toBeVisible()
+  await expect(page.getByText('PW bio Test')).toBeVisible()
+  await expect(page.getByText('12341234')).toBeVisible()
+
+  // Edit user tests
+  await page.goto('http://localhost:3000/Users/');
+  await expect(page).toHaveTitle('User Data Home Page');
+  await expect(page.getByText('PW username Test')).toBeVisible()
+  await page
+    .getByRole('list')
+    .filter({hasText: 'PW username Test'})
+    .getByText('Update')
+    .click()
+  await expect(page).toHaveTitle('Update User Data');
+  await page.getByLabel('Update username: ').fill('PW username edit Test')
+  await page.getByLabel('Update bio: ').fill('PW bio edit Test')
+  await page.getByLabel('Update age: ').fill('1234123456')
+  await page.getByText('Update Post').click()
+  await expect(page.getByText('edited')).toBeVisible()
+
+  await page.goto('http://localhost:3000/Users');
+  await expect(page.getByText('PW username edit Test')).toBeVisible()
+  await expect(page.getByText('PW bio edit Test')).toBeVisible()
+  await expect(page.getByText('1234123456')).toBeVisible()
+
+  // Delete user tests
+  await page.goto('http://localhost:3000/Users/');
+  await expect(page).toHaveTitle('User Data Home Page');
+  await expect(page.getByText('PW username edit Test')).toBeVisible()
+  await page
+    .getByRole('list')
+    .filter({hasText: 'PW username edit Test'})
+    .getByText('Delete')
+    .click()
+    await expect(page).toHaveTitle('Delete User Data');
+  await page.getByText('Delete Post').click()
+  await expect(page.getByText('deleted')).toBeVisible()
+
+  await page.goto('http://localhost:3000/Users');
+  await expect(page.getByText('PW username edit Test')).toBeVisible({visible: false})
+
+});
 
 
-
-
-// test('get started link', async ({ page }) => {
-//   await page.goto('https://playwright.dev/');
-
-//   // Click the get started link.
-//   await page.getByRole('link', { name: 'Get started' }).click();
-
-//   // Expects page to have a heading with the name of Installation.
-//   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-// });
